b19291c903ef7fd9d3db845f3db56ac362a69110
ChiquitoS11 <142192480+ChiquitoS11@users.noreply.github.com>
2024-03-11 12:31:36 +0100

    Create NuevaFuncionalidad.java

---
+++ indexMonasChinas/src/main/java/full/test/indexmonaschinas/NuevaFuncionalidad.java
@@ -0,0 +1,22 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package full.test.indexmonaschinas;
+
+/**
+ *
+ * @author test
+ */
+public class NuevaFuncionalidad {
+
+    String nombre;
+    
+    public NuevaFuncionalidad(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void escribirNombreMonaChina(){
+        System.out.println(nombre);
+    }
+}
